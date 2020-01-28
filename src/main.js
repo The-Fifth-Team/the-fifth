ac0f2d0c4d6f3c93929a7c2a8971b18e03b5e5f4
alijalal-sys <ajmabilal@gmail.com>
2020-01-28 15:49:12 +0100

    sidebar basic structure

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import DashboardPlugin from '@/plugins/dashboard-plugins'
+import Vuesax from 'vuesax'
+
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+Vue.use(Vuesax)
 
 import router from "./router/index";
 import "vue-notifyjs/themes/default.css";
 
-Vue.use(DashboardPlugin)
+import 'material-icons/iconfont/material-icons.css';
+
 Vue.config.productionTip = false
 
 new Vue({
