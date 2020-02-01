3bef0640e912b25fe2859411a40a9b42a085031a
Ahmed Wheida <ahmed.wheida@gmail.com>
2020-02-01 16:44:34 +0100

    Added fxnl component for adding user face descriptors.

--- src/components/webcam-2.vue
+++ src/components/webcam-2.vue
@@ -31,121 +31,119 @@
     >
       Turn Camera Off
     </button>
+    <p id="output" />
   </section>
 </template>
 
-
-<!-- <script type="module" src="../assets/face-api.min.js"></script> -->
-
-
 <script lang="js">
+import * as faceapi from "../../public/face-api.min.js"
+const axios = require('axios');
 
+export default {
+  name: 'Webcam2',
+  props: [],
+  data() {
+    return {}
+  },
+  computed: {
 
+  },
+  mounted() {
 
-const faceapi = fetch("../assets/face-api.min")
-
-
-// const faceapi = require('../assets/face-api.min')
-
-// const faceapi = require('face-api.js');
-
-
-
-//import * as faceapi from "../assets/face-api.min.js"
-
-  export default  {
-    name: 'Webcam2',
-    props: [],
-    data () {
-      return {
-
-
-      }
-    },
-    computed: {
-
-    },
-    mounted () {
-
-    },
-    methods: {
-      startVideo: () => {
+  },
+  methods: {
+    startVideo: () => {
       const video = document.getElementById('video1')
+      navigator.getUserMedia({
+          video: {}
+        },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+      )
+    },
+    captureVideoButton: () => {
+      const video = document.getElementById('video1')
+      const output = document.getElementById('output')
 
-       navigator.getUserMedia(
-         { video: {} },
-         stream => video.srcObject = stream,
-         err => console.error(err)
-       )
-     },
-      captureVideoButton:() => {
-        const video = document.getElementById('video1')
-
-
-        Promise.all([
-          faceapi.nets.tinyFaceDetector.loadFromUri('../assets/models'),
-          faceapi.nets.faceLandmark68Net.loadFromUri('../assets/models'),
-          faceapi.nets.faceRecognitionNet.loadFromUri('../assets/models'),
-          faceapi.nets.faceExpressionNet.loadFromUri('../assets/models')
+      Promise.all([
+          faceapi.nets.ssdMobilenetv1.loadFromUri('./models'),
+          faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+          faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+          faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+          faceapi.nets.faceExpressionNet.loadFromUri('./models')
         ])
+        .then(() => {
+          startRecognizing()
+        });
 
+      var startRecognizing = () => {
+        console.log(video.readyState)
+        navigator.getUserMedia({
+            video: {}
+          },
+          stream => video.srcObject = stream,
+          err => console.error(err)
+        )
+        video.addEventListener('playing', () => {
+          const canvas = faceapi.createCanvasFromMedia(video)
+          document.body.append(canvas)
+          const displaySize = {
+            width: video.width,
+            height: video.height
+          }
+          faceapi.matchDimensions(canvas, displaySize)
+          //clearInterval(refreshId);
+          var refreshId = setTimeout(async () => {
+            const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceExpressions().withFaceDescriptors()
 
-        // .then(startRecognizing).catch((err) => {
-        //   console.log({err})
-        // });
-
-        var startRecognizing = () => {
-          console.log(video.readyState)
-          navigator.getUserMedia(
-            { video: {} },
-            stream => video.srcObject = stream,
-            err => console.error(err)
-          )
-          video.addEventListener('playing', () => {
-            const canvas = faceapi.createCanvasFromMedia(video)
-            document.body.append(canvas)
-            const displaySize = { width: video.width, height: video.height }
-            faceapi.matchDimensions(canvas, displaySize)
-            setInterval(async () => {
-              const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-              if (detections) {
-                for (var i = 0; i < detections.length; i++) {
-                  console.log(Object.values(detections[0].expressions))
-                }
-              }
-              const resizedDetections = faceapi.resizeResults(detections, displaySize)
-              canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-              faceapi.draw.drawDetections(canvas, resizedDetections)
-              faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-              faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-            }, 1000)
-          })
-
-        }
-
-      },
-      takeScreenShot: () => {
-        const video = document.getElementById('video1')
-        const canvas = document.getElementById('canvas1');
-        const img = document.getElementById('screenshot');
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        canvas.getContext('2d').drawImage(video, 0, 0);
-      },
-      turnCameraOff : () => {
-        const video = document.getElementById('video1')
-          let stream = video.srcObject;
-          let tracks = stream.getTracks()[0].stop();
+            if (detections) {
+              axios({
+                  method: 'post',
+                  url: 'http://localhost:3000/',
+                  data: detections,
+                  headers: {
+                    'Content-Type': 'application/json'
+                  }
+                })
+                .then(function(response) {
+                  console.log({response});
+                })
+                .catch(function(error) {
+                  console.log({error});
+                });
+            }
+            const resizedDetections = faceapi.resizeResults(detections, displaySize)
+            canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+            faceapi.draw.drawDetections(canvas, resizedDetections)
+            faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+            faceapi.draw.drawFaceExpressions(canvas, resizedDetections,0.05 )
+            }, 3000)
+        })
       }
+    },
+    takeScreenShot: () => {
+      const video = document.getElementById('video1')
+      const canvas = document.getElementById('canvas1');
+      const img = document.getElementById('screenshot');
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      canvas.getContext('2d').drawImage(video, 0, 0);
 
+      // video.addEventListener('ended', (event) => {
+      //   console.log('Video stopped either because 1) it was over, ' +
+      //     'or 2) no further data is available.');
+      // });
+    },
+    turnCameraOff: () => {
+      const video = document.getElementById('video1')
+      let stream = video.srcObject;
+      let tracks = stream.getTracks()[0].stop();
     }
+
+  }
 }
-
-
 </script>
 
 <style scoped lang="scss">
-  .webcam-2 {
-
-  }
+.webcam-2 {}
 </style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,16 @@
 // import Dashboard from '../pages/Dashboard'
 import webcam2 from '../components/webcam-2'
-
+import facialDiscriptorExtractor from '../components/facialDiscriptorExtractor'
 const routes = [
   {
     path: '/',
     name: 'Dashboard',
     component: webcam2
+  },
+  {
+    path: '/descriptor',
+    name: 'descriptor',
+    component: facialDiscriptorExtractor
   }
 ];
 
