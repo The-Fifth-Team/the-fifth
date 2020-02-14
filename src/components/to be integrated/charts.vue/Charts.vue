941a310f5065619679cdd3c3f06998e261e0a58b
Soug-crypto <56478942+Soug-crypto@users.noreply.github.com>
2020-02-14 18:02:34 +0100

    added query for Charts.vue

--- src/components/to be integrated/charts.vue/Charts.vue
+++ src/components/to be integrated/charts.vue/Charts.vue
@@ -56,8 +56,7 @@ export default {
     };
   },
   created() {
-    updatedOptions = Object.create(this.cd)
-
+    let updatedOptions = Object.create(this.cd)
     this.$apollo.query({
       query: getPeriodEmotions,
       variables: {
@@ -65,30 +64,18 @@ export default {
         endDate: Date.now().toString()
       }
     }).then((results) => {
-
-      // var timeTable = ["0:00", "0:15", "0:30", "0:45", "1:00", "1:15", "1:30", "1:45", "2:00", "2:15", "2:30", "2:45", "3:00", "3:15", "3:30", "3:45", "4:00", "4:15", "4:30", "4:45",
-      //   "5:00", "5:15", "5:30", "5:45", "6:00", "6:15", "6:30", "6:45", "7:00", "7:15", "7:30", "7:45", "8:00", "8:15", "8:30", "8:45", "9:00", "9:15", "9:30", "9:45",
-      //   "10:00", "10:15", "10:30", "10:45", "11:00", "11:15", "11:30", "11:45", "12:00", "12:15", "12:30", "12:45", "13:00", "13:15", "13:30", "13:45", "14:00", "14:15", "14:30", "14:45",
-      //    "15:00", "15:15", "15:30", "15:45", "16:00", "16:15", "16:30", "16:45", "17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45",
-      //   "20:00", "20:15", "20:30", "20:45", "21:00", "21:15", "21:30", "21:45", "22:00", "22:15", "22:30", "22:45", "23:00", "23:15", "23:30", "23:45", "24:00"]
-      //
-      // var date = new Date(Date.now() - (24 * 60 * 60 * 1000))
-      // var timeStamp = date.getHours() + ":" + date.getMinutes()
-      // Date.parse('01/01/2011 '+timeStamp+":00")
-      // timeTable.forEach((val) => {
-      //   console.log(Date.parse('01/01/2011 '+timeStamp+":00")>Date.parse('01/01/2011 '+val+":00"))
-      // })
-
       let fetchedData = []
-      results.forEach((emotionEntry, i) => {
+      results['averages'].forEach((emotionEntry, i) => {
+        let time = new Date(parseInt(results['timeStamps'][i]))
+        let timeStamp = time.getHours() + ":" + time.getMinutes()
         Array.prototype.push.apply(fetchedData, [
-          ["TimeStamp", emotionEntry[0], "Neutral"],
-          ["TimeStamp", emotionEntry[1], "Happy"],
-          ["TimeStamp", emotionEntry[2], "Sad"],
-          ["TimeStamp", emotionEntry[3], "Angry"],
-          ["TimeStamp", emotionEntry[4], "Fearful"],
-          ["TimeStamp", emotionEntry[5], "Disgusted"],
-          ["TimeStamp", emotionEntry[6], "Surprised"]
+          [timeStamp, emotionEntry[0], "Neutral"],
+          [timeStamp, emotionEntry[1], "Happy"],
+          [timeStamp, emotionEntry[2], "Sad"],
+          [timeStamp, emotionEntry[3], "Angry"],
+          [timeStamp, emotionEntry[4], "Fearful"],
+          [timeStamp, emotionEntry[5], "Disgusted"],
+          [timeStamp, emotionEntry[6], "Surprised"]
         ])
       })
       updatedOptions['river']['series'][0]['data'] = fetchedData
