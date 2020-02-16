f0742ed9d755dde65be5ff34dabb9c7a16d47c33
Soug-crypto <56478942+Soug-crypto@users.noreply.github.com>
2020-02-16 21:08:04 +0100

    updates to try for pwa

---
+++ src/components/to be integrated/pwa file additions/vue.config.js
@@ -0,0 +1,23 @@
+const manifestJSON = require('./public/manifest.json')
+module.exports = {
+  pwa: {
+    themeColor: manifestJSON.theme_color,
+    name: manifestJSON.short_name,
+    msTileColor: manifestJSON.background_color,
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'service-worker.js',
+    },
+  },
+
+
+
+
+
+//  pwa: {
+  //  workboxOptions: {
+  //    navigateFallback: 'index.html'
+    //}
+  //}
